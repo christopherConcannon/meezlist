77e818d2cd20622b0e0e95804fd9a74333826ca5
xtophe xanon <xocon10@gmail.com>
2021-02-02 15:31:08 -0600

    create user and listing models

---
+++ server/data/listings.js
@@ -0,0 +1,86 @@
+export const listingData = [
+	{
+		title       : 'listing one',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing1_1.jpg',
+			'/images/listing1_2.jpg',
+			'/images/listing1_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	},
+	{
+		title       : 'listing two',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing2_1.jpg',
+			'/images/listing2_2.jpg',
+			'/images/listing2_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	},
+	{
+		title       : 'listing three',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing3_1.jpg',
+			'/images/listing3_2.jpg',
+			'/images/listing3_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	},
+	{
+		title       : 'listing four',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing4_1.jpg',
+			'/images/listing4_2.jpg',
+			'/images/listing4_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	},
+	{
+		title       : 'listing five',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing5_1.jpg',
+			'/images/listing5_2.jpg',
+			'/images/listing5_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	},
+	{
+		title       : 'listing six',
+		description :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, distinctio a. Pariatur nostrum eos, delectus amet commodi illum, explicabo maxime fugit quasi illo doloremque voluptates. Repellat dignissimos iste in aut',
+		images      : [
+			'/images/listing6_1.jpg',
+			'/images/listing6_2.jpg',
+			'/images/listing6_3.jpg'
+		],
+		brand       : 'Takeda',
+		category    : 'knives',
+		location    : 'Austin, Tx',
+		price       : 100.99
+	}
+]
---
+++ server/data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcrypt'
+
+const users = [
+  {
+    name: 'wacko',
+    email: 'wacko@gmail.com',
+    password: bcrypt.hashSync('password123', 10)
+  },
+  {
+    name: 'jacko',
+    email: 'jacko@gmail.com',
+    password: bcrypt.hashSync('password123', 10)
+  },
+  {
+    name: 'smacko',
+    email: 'smacko@gmail.com',
+    password: bcrypt.hashSync('password123', 10)
+  },
+]
+
+export default users
